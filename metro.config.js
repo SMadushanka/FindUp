c50e85defd7d9880b7ce73c2e251eb4c3f271474
Sampath Madushanka <wijamu.maduu22@gmail.com>
2026-02-01 09:17:52 +0530

    fix: update input path for withNativeWind in metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
+import { *asImagePicker } from 'expo-image-picker';
 const { withNativeWind } = require('nativewind/metro');
  
 const config = getDefaultConfig(__dirname)
  
-module.exports = withNativeWind(config, { input: './global.css' })
+module.exports = withNativeWind(config, { input: './global.css/*asImagePicker' })
