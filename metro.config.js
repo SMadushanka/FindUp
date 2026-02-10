19e2152f7bc693485d869588bd6968e9c6fdaba7
Sampath Madushanka <wijamu.maduu22@gmail.com>
2026-02-10 11:06:17 +0530

    change code

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
-import { *asImagePicker } from 'expo-image-picker';
 const { withNativeWind } = require('nativewind/metro');
  
 const config = getDefaultConfig(__dirname)
  
-module.exports = withNativeWind(config, { input: './global.css/*asImagePicker' })
+module.exports = withNativeWind(config, { input: './global.css' })
